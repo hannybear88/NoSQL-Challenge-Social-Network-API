6e4f75e67eb806e8d4a775739bbb9180387099e0
hannybear88 <hannahkchung88@gmail.com>
2023-02-07 12:15:01 -0800

    added code in server.js file

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+const express = require("express");
+const app = express();
+const db = require("./config/connection")
+const routes = require("./controllers")
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(routes)
+
+db.once("open", () => {
+    app.listen(3001, () => {
+        console.log("Server is running on PORT 3001!")
+    })
+})
 
