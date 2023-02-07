cda711fd8008db356f340d8e43cc3ef391282eea
hannybear88 <hannahkchung88@gmail.com>
2023-02-07 12:12:56 -0800

    added code in user-contorller.js file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,58 +1,83 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Thought } = require("../models");
 
+// DBEUG CONST
+const DEBUG_DELETE_THOUGHTS = true;
+
+// Get all users
 router.get("/", (req, res) => {
-  try {
-    User.find().then((result) => {
-      res.json(result);
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		User.find().then((result) => {
+			res.json(result);
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// Get a user
 router.get("/:id", (req, res) => {
-  try {
-    User.findById(req.params.id)
-      .populate("thoughts")
-      .populate("friends")
-      .then((result) => {
-        res.json(result);
-      });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		User.findById(req.params.id)
+			.populate("thoughts")
+			.populate("friends")
+			.then((result) => {
+				res.json(result);
+			});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
+// Post a new user
 router.post("/", (req, res) => {
-  User.create(req.body).then((result) => {
-    res.json(result);
-  });
+	User.create(req.body).then((result) => {
+		res.json(result);
+	});
 });
 
+// Put an update to a user by _id
 router.put("/:id", (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  }).then((result) => {
-    res.json(result);
-  });
+	User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	}).then((result) => {
+		res.json(result);
+	});
 });
 
+// Delete the user by _id
 router.delete("/:id", (req, res) => {
-  User.findByIdAndDelete(req.params.id).then((result) => {
-    res.json(result);
-  });
+	User.findByIdAndDelete(req.params.id).then((result) => {
+		res.json(result);
+	});
 });
 
-
+// Function to delete user's associated thoughts
 
 // ADD FRIEND
-// router.post("/:userId/friends/:friendId", (req, res) => {
-
-// })
-
-
+// Post a new friend to user data
+router.post("/:userId/friends/:friendId", (req, res) => {
+	const update = {
+		$push: {
+			reactions: req.params.friendId,
+		},
+	};
+	User.findByIdAndUpdate(req.params.userId, update, {new: true,}).then((result) => {
+		res.json(result);
+	});
+});
 
 // DELETE FRIEND
+// Delete a friend from the user's friend list
+router.delete("/:userId/friends/:friendId", (req, res) => {
+	const update = {
+		$push: {
+			reactions: req.params.friendId,
+		},
+	};
+	User.findByIdAndUpdate(req.params.userId, update, {new: true,}).then((result) => {
+		res.json(result);
+	});
+});
 
 module.exports = router;
