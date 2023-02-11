21397c95a6b449bb362ff6fbfbfbe60cd7fd32a0
hannybear88 <hannahkchung88@gmail.com>
2023-02-10 18:17:59 -0800

    refactored code in index.js file in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
+const userRoutes = require("./user-controller")
 
-router.use("/api", apiRoutes);
+router.use("/api/user", userRoutes);
 
-router.use((req, res) => {
-  res.status(404).send("<h1>😝 404 Error!</h1>");
-});
+router.use("*", (req, res) => {
+    res.send("Wrong route!!")
+})
 
 module.exports = router;
+
+
+
+// const router = require("express").Router();
+// const apiRoutes = require("./api");
+
+// router.use("/api", apiRoutes);
+
+// router.use((req, res) => {
+//   res.status(404).send("<h1>😝 404 Error!</h1>");
+// });
+
+// module.exports = router;
