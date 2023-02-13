c7e087dd14736e77c730eecee285c4ad1927558b
hannybear88 <hannahkchung88@gmail.com>
2023-02-13 15:13:14 -0800

    modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,7 @@
 const router = require("express").Router();
-const userRoutes = require("./user-controller")
+const { userRoutes, thoughtRoutes } = require("./api")
 
 router.use("/api/user", userRoutes);
-
-router.use("*", (req, res) => {
-    res.send("Wrong route!!")
-})
+router.use("/api/thought", thoughtRoutes);
 
 module.exports = router;
-
-
-
-// const router = require("express").Router();
-// const apiRoutes = require("./api");
-
-// router.use("/api", apiRoutes);
-
-// router.use((req, res) => {
-//   res.status(404).send("<h1>😝 404 Error!</h1>");
-// });
-
-// module.exports = router;
