9ff592cf72389275f775b36f3d62e3de8df05620
hannybear88 <hannahkchung88@gmail.com>
2023-02-13 15:08:19 -0800

    changed some code in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,4 @@
-const router = require("express").Router();
-const userRoutes = require("./user-controller")
-const thoughtRoutes = require("./thought-controller")
+const userController = require("./user-controller");
+const thoughtController = require("./thought-controller");
 
-router.use("/api/user", userRoutes);
-router.use("/api/thought", thoughtRoutes);
-
-router.use("*", (req, res) => {
-    res.send("Wrong route!!")
-})
-
-module.exports = router;
+module.exports = {userController, thoughtController}
