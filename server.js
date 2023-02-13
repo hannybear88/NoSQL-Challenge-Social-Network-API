69e064ac7f7f65430b84306113328dcfbccd8ba4
hannybear88 <hannahkchung88@gmail.com>
2023-02-13 15:14:13 -0800

    modified server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const db = require("./config/connection")
-const routes = require("./controllers")
+const db = require("./config/connection");
+const routes = require("./routes");
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
